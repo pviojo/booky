7d38ceec4af626d3883d745cd2ae833a8ad5b423
Pablo Viojo <pviojo@gmail.com>
2023-07-30 19:29:14 -0400

    Changes in eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable import/extensions */
 import "./globals.scss";
-import pjson from '../../package.json';
 
-import type { Metadata } from "next";
-import { IBM_Plex_Sans, Roboto_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Roboto_Mono} from "next/font/google";
 
-import { config } from "@fortawesome/fontawesome-svg-core";
+import {config} from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Providers from './providers';
-import ThemeButton from '@/components/ThemeButton';
-import Script from 'next/script';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import Script from "next/script";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import ThemeButton from "@/components/ThemeButton";
+import Providers from "./providers";
+
+import pjson from "../../package.json";
+
 config.autoAddCss = false;
 
 const typography = Roboto_Mono({
@@ -23,47 +26,47 @@ export const metadata: Metadata = {
   description: "Simple Bookmark Tool",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${typography.className}
           subpixel-antialiased`}
       >
-        {process?.env?.NODE_ENV === 'production' ?
-          <><Script src="https://www.googletagmanager.com/gtag/js?id=G-T9M1XZ8MZP"></Script>
-            <Script id="google-analytics">
+        {process?.env?.NODE_ENV === "production" ? (
+          <>
+            <Script src='https://www.googletagmanager.com/gtag/js?id=G-T9M1XZ8MZP' />
+            <Script id='google-analytics'>
               {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-T9M1XZ8MZP');`}
             </Script>
-          </> : null}
+          </>
+        ) : null}
         <Providers>
-          <div className="flex justify-between p-3 px-3  md:px-10">
+          <div className='flex justify-between p-3 px-3  md:px-10'>
             <div />
             <div>
               <ThemeButton />
             </div>
           </div>
           {children}
-          <div className="mt-20 mb-5">
-            <div className="flex items-end justify-between p-3 inner px-11">
+          <div className='mt-20 mb-5'>
+            <div className='flex items-end justify-between p-3 inner px-11'>
               <div className='text-xs'>
-                <FontAwesomeIcon icon={faHeart}
-                  className='mr-2 color-purple'
-                />
+                <FontAwesomeIcon icon={faHeart} className='mr-2 color-purple' />
                 Made with Next.js, TypeScript and Tailwind CSS
                 <br />
                 <br />
-                Fork on
-                {' '}
-                <a className="underline" href="https://github.com/pviojo/booky" target='_blank' rel="noreferrer">
+                Fork on{" "}
+                <a
+                  className='underline'
+                  href='https://github.com/pviojo/booky'
+                  target='_blank'
+                  rel='noreferrer'
+                >
                   GitHub
                 </a>
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,62 @@
-import FormBookmark from '@/components/FormBookmark';
-import { getBookmarksForAuthor, getTagCloudForAuthor } from '@/lib/database';
-import Bookmark from '@/components/Bookmark';
-import TagCloud from '@/components/TagCloud';
+import FormBookmark from "@/components/FormBookmark";
+import {getBookmarksForAuthor, getTagCloudForAuthor} from "@/lib/database";
+import Bookmark from "@/components/Bookmark";
+import TagCloud from "@/components/TagCloud";
 
 const AUTHOR_ID = 1;
 
 export default async function Home({
-  searchParams
+  searchParams,
 }: {
-  searchParams: { t: string }
+  searchParams: {t: string};
 }) {
   const bookmarks = await getBookmarksForAuthor(AUTHOR_ID);
   const tagCloud = await getTagCloudForAuthor(AUTHOR_ID);
-  console.log('tagCloud', tagCloud)
 
-  const isAuthenticated = process.env.NODE_ENV === 'development' || searchParams?.t === process.env.ACCESS_TOKEN;
+  const isAuthenticated =
+    process.env.NODE_ENV === "development" ||
+    searchParams?.t === process.env.ACCESS_TOKEN;
 
   return (
-    <div className="inner">
-      <div className="px-5 md:px-10 ">
+    <div className='inner'>
+      <div className='px-5 md:px-10 '>
         <div className='page-title'>Bookmarks</div>
         <div className='grid text- grid-cols-[1fr_300px] gap-10'>
           <div>
             {isAuthenticated && <FormBookmark authorId={AUTHOR_ID} />}
             <div className='mt-10'>
-              {bookmarks?.length ?
-                <>{
-                  bookmarks.map((bookmark, i) => <div className="gap-6 flex py-4 [&:not(:last-child)]:border-b border-gray" key={bookmark.id} >
-                    <div style={{ width: 40, height: 40 }} className="items-center justify-center inline-block mt-1 text-sm bg-gray-200 rounded-full grid aspect-square dark:bg-gray-500"><span>{i + 1}</span></div>
-                    <div className="flex-auto">
-                      <Bookmark bookmark={bookmark} isAuthenticated={isAuthenticated} />
+              {bookmarks?.length ? (
+                <>
+                  {bookmarks.map((bookmark, i) => (
+                    <div
+                      className='gap-6 flex py-4 [&:not(:last-child)]:border-b border-gray'
+                      key={bookmark.id}
+                    >
+                      <div
+                        style={{width: 40, height: 40}}
+                        className='items-center justify-center  mt-1 text-sm bg-gray-200 rounded-full grid aspect-square dark:bg-gray-500'
+                      >
+                        <span>{i + 1}</span>
+                      </div>
+                      <div className='flex-auto'>
+                        <Bookmark
+                          bookmark={bookmark}
+                          isAuthenticated={isAuthenticated}
+                        />
+                      </div>
                     </div>
-                  </div>)
-                }
+                  ))}
                 </>
-                : <div>No bookmarks yet!</div>
-              }
+              ) : (
+                <div>No bookmarks yet!</div>
+              )}
             </div>
-
           </div>
           <div>
             <TagCloud tags={tagCloud} />
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
